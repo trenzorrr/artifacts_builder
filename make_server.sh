94f224b8c4e90bb0ecb9aa86922d69bdb0a978bf
trenzorrr <treznor@programmer.net>
2019-01-08 12:04:02 +0100

    fix server script os check

--- make_server.sh
+++ make_server.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 ## skip server build for osx
-if "$TRAVIS_OS_NAME" = linux; then
-    docker exec -it buildct /build/xmrigCCServer-dynamic-build.sh || exit 1
-fi
+case "$TRAVIS_OS_NAME" in
+    linux)
+        docker exec -it buildct /build/xmrigCCServer-dynamic-build.sh || exit 1
+        ;;
+esac
