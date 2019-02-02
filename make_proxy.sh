0abd289025054e21b1344561d96f922badad1b54
trenzorrr <treznor@programmer.net>
2019-02-02 18:49:04 +0100

    added build support for proxy and bridge

---
+++ make_proxy.sh
@@ -0,0 +1,11 @@
+## skip server build for osx
+case "$TRAVIS_OS_NAME" in
+    linux)
+        if [ "${TRAVIS_TAG/proxy}" != "$TRAVIS_TAG" ]; then
+            docker exec -it buildct_ubu bash /build/build.sh || exit 1
+            cd xnp
+            zip xnp.zip -x '.git*' -x 'README*' -r .
+            cd -
+        fi
+        ;;
+esac
