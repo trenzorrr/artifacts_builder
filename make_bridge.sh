14e076282fedf8301b93f6165b06282a3223603e
trenzorrr <treznor@programmer.net>
2019-02-03 08:55:30 +0100

    don't dereference symlinks with zip

--- make_bridge.sh
+++ make_bridge.sh
@@ -1,7 +1,7 @@
 ## skip server build for osx
 case "$TRAVIS_OS_NAME" in
     linux)
-        docker exec -it buildct_ubu bash /build/build_mm.sh || exit 1
-        zip mm.zip mm
+        docker exec -it buildct_ubu /build/build_mm.sh || exit 1
+        zip mm.zip mm -y
         ;;
 esac
--- make_proxy.sh
+++ make_proxy.sh
@@ -4,7 +4,7 @@ case "$TRAVIS_OS_NAME" in
         if [ "${TRAVIS_TAG/proxy}" != "$TRAVIS_TAG" ]; then
             docker exec -it buildct_ubu bash /build/build.sh || exit 1
             cd xnp
-            zip xnp.zip -x '.git*' -x 'README*' -r .
+            zip xnp.zip -x '.git*' -x 'README*' -r . -y
             cd -
         fi
         ;;
