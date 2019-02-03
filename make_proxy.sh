d091883012de005d248bf733a3e12faf24fbd684
trenzorrr <treznor@programmer.net>
2019-02-03 17:23:01 +0100

    changed tags tail regex check

--- make_proxy.sh
+++ make_proxy.sh
@@ -1,11 +1,9 @@
 ## skip server build for osx
 case "$TRAVIS_OS_NAME" in
     linux)
-        if [ "${TRAVIS_TAG/proxy}" != "$TRAVIS_TAG" ]; then
-            docker exec -it buildct_ubu bash /build/build.sh || exit 1
-            cd xnp
-            zip xnp.zip -x '.git*' -x 'README*' -r . -y
-            cd -
-        fi
+        docker exec -it buildct_ubu bash /build/build.sh || exit 1
+        cd xnp
+        zip xnp.zip -x '.git*' -x 'README*' -r . -y
+        cd -
         ;;
 esac
