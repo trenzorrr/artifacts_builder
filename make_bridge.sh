0bd2e942b6ed11d9ba521f759e89190b33c9c23f
trenzorrr <treznor@programmer.net>
2019-02-03 08:25:50 +0100

    refactored scripts fetching

--- make_bridge.sh
+++ make_bridge.sh
@@ -1,9 +1,7 @@
 ## skip server build for osx
 case "$TRAVIS_OS_NAME" in
     linux)
-        if [ "${TRAVIS_TAG/bridge}" != "$TRAVIS_TAG" ]; then
-            docker exec -it buildct_ubu bash /build/build_mm.sh || exit 1
-            zip mm.zip mm
-        fi
+        docker exec -it buildct_ubu bash /build/build_mm.sh || exit 1
+        zip mm.zip mm
         ;;
 esac
